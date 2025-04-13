41704b35002666a7b61a39cd7362f2f130621e64
Hassan <muhammad.hassan@antematter.io>
2025-04-14 01:01:16 +0500

    added unit test for system program cpi

--- tests/program-a.ts
+++ tests/program-a.ts
@@ -1,16 +1,38 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { ProgramA } from "../target/types/program_a";
+import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-describe("program-a", () => {
-  // Configure the client to use the local cluster.
+async function airdrop(connection: anchor.web3.Connection, payer: PublicKey, amount: number) {
+  const signature = await connection.requestAirdrop(
+    payer,
+    amount * LAMPORTS_PER_SOL
+  );
+  let latestBlockhash = await connection.getLatestBlockhash();
+  return await connection.confirmTransaction({
+    signature: signature,
+    ...latestBlockhash
+  });
+}
+
+
+describe("Cross program invocation", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.ProgramA as Program<ProgramA>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  function getProgramWithProvider(provider: anchor.AnchorProvider): anchor.Program {
+    return new anchor.Program(program.idl as anchor.Idl, provider);
+  }
+
+  it("Should transfer SOL using CPI with system program", async () => {
+    const [pdaAccountAddress, pdaAccountBump] = await anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("pda"), program.provider.publicKey.toBuffer()],
+      program.programId
+    );
+    
+    await airdrop(program.provider.connection, pdaAccountAddress, 10);
+
+    const tx = await program.methods.cpiSystemProgram().rpc();
     console.log("Your transaction signature", tx);
   });
 });
